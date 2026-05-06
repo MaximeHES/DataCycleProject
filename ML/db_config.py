7df6402949f8cebe888fda1ef61aed9468875359
MaximeHES <117347361+MaximeHES@users.noreply.github.com>
2026-05-06 11:45:39 +0200

    Hardcoded credentials removal and replace with azure key vault

--- ML/db_config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "server": "sql-eversys-coffee.database.windows.net",
-    "database": "sql-db-eversys-gold",
-    "username": "sqladmin",
-    "password": "AdminHevs01",
-    "driver": "ODBC Driver 17 for SQL Server"
-}
